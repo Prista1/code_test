fb8c052b4a7552e946322e6a6255e0fb79bad8f2
Brian Deterling <brian.deterling@deterling.net>
2011-02-07 09:06:56 -0600

    My solution

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,13 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+jQuery(function($) {
+  $('.patient_need').click(function() {
+    var el = $(this);
+    var patient_id = el.attr('id').split('_')[1]; 
+    var need_id = el.attr('id').split('_')[2]; 
+    jQuery.post('/set_patient_need/' + patient_id + '/' + need_id, function(data) {
+      el.html('true');
+    });
+  });
+});

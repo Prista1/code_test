dcbc5af89324c5fd2449b55d20ad10c76c1ee7de
Paul Anunda <paulanunda@gmail.com>
2011-02-14 10:30:38 -0600

    created table view

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,8 @@
 class WelcomeController < ApplicationController
 
   def index
+    @patients = Patient.all.sort_by{|p| p.name.downcase}
+    @needs = Need.all.sort_by{|p| p.name.downcase}
   end
 
   def set_care_need

5f29ed7ba7bb08041d51112091b3586426fa2a43
Paul Anunda <paulanunda@gmail.com>
2011-02-14 12:31:35 -0600

    Created patient grid

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,11 +1,19 @@
 class WelcomeController < ApplicationController
 
   def index
+    # Load all patients
     @patients = Patient.all.sort_by{|p| p.name.downcase}
+    # Load all needs
     @needs = Need.all.sort_by{|p| p.name.downcase}
   end
 
   def set_care_need
+    #find patient
+    @patient = Patient.find_by_id(params[:patient_id])
+    #find id
+    @need = Need.find_by_id(params[:need_id])
+    # creates need for patient
+    @patient.needs << @need
   end
-
+  
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,4 +2,5 @@
 // This file is automatically included by javascript_include_tag :defaults
 
 jQuery(function($) {
+	
 });
